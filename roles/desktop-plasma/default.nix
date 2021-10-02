3a5c449d61c36519a7e8b2ea06707c1ef67dfbf2
Peter Storm <peter.storm@peterstorm.io>
2021-10-02 18:56:35 +0200

    half working

--- roles/desktop-plasma/default.nix
+++ roles/desktop-plasma/default.nix
@@ -7,9 +7,18 @@
     enable = true;
     layout = "us";
     xkbOptions = "caps:none, caps:hyper";
+    displayManager = {
+      defaultSession = "none+xmonad";
+      sddm.enable = true;
+    };
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+    };
+    desktopManager = {
+      plasma5.enable = true;
+    };
   };
-  services.xserver.displayManager.defaultSession = "none+xmonad";
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+
 }
 
