40cddb3b11ab8473114964f1bf0be94484f5364d
Peter Storm <peter.storm@peterstorm.io>
2023-11-13 14:52:58 +0100

    override existing `cloudflared` package to use `warp-routing`

--- roles/cloudflared/default.nix
+++ roles/cloudflared/default.nix
@@ -1,11 +1,17 @@
 {config, pkgs, lib, ...}:
 
 {
+  disabledModules = [ "services/networking/cloudflared.nix" ];
+
+  imports = [
+    ./myCloudflared.nix
+  ];
+
   services.cloudflared = {
     enable = true;
     tunnels = {
       "206b7a4a-a658-437d-a98b-c14c6e4cc286" = {
-        credentialsFile = "/home/peterstorm/.cloudflared/206b7a4a-a658-437d-a98b-c14c6e4cc286.json";
+        credentialsFile = "/var/lib/cloudflared/206b7a4a-a658-437d-a98b-c14c6e4cc286.json";
         warp-routing.enabled = true;
         default = "http_status:404";
       };
