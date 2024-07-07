95f891053f682043d8b96232eb02ab493c63b27c
Peter Storm <peter.storm@peterstorm.io>
2024-07-07 14:53:57 +0200

    mwaybe fix mason

--- roles/home-manager/core-apps/neovim/lua/plugins/plugins.lua
+++ roles/home-manager/core-apps/neovim/lua/plugins/plugins.lua
@@ -86,6 +86,15 @@ return {
   },
   {
     'williamboman/mason.nvim',
+    opts = {
+      ui = {
+        icons = {
+          package_installed = '✓',
+          package_pending = '➜',
+          package_uninstalled = '✗',
+        },
+      },
+    },
   },
   {
     'nvim-java/nvim-java'
