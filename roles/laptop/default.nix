1cc9d584a981568e7503e463ff800f6bded7bcd7
Peter Storm <peter.storm@peterstorm.io>
2024-09-14 19:07:15 +0200

    stuff

--- roles/laptop/default.nix
+++ roles/laptop/default.nix
@@ -3,7 +3,6 @@
   services.thermald.enable = lib.mkDefault true;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.opengl = {
-    driSupport = true;
     driSupport32Bit = true;
   };
 }
--- roles/wifi/default.nix
+++ roles/wifi/default.nix
@@ -4,7 +4,7 @@
  networking.networkmanager.enable = true;
  networking.firewall = {
   enable = true;
-  # allowedTCPPorts = [ 8010 6443 80 443 ];
+    allowedTCPPorts = [ 8081 ];
  };
  services.avahi.enable = true;
 }
