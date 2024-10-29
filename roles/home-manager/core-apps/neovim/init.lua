10fa1ca905e99b36216141a8dffa4aa75d7d5f2a
Peter Storm <peter.storm@peterstorm.io>
2024-10-29 13:51:39 +0100

    change pattern in Neoformat pre save

--- roles/home-manager/core-apps/neovim/init.lua
+++ roles/home-manager/core-apps/neovim/init.lua
@@ -164,6 +164,6 @@ require('lspconfig').tailwindcss.setup({})
 g.neoformat_try_node_exe = 1
 
 api.nvim_create_autocmd("BufWritePre", {
-  pattern = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  pattern = { "*.js", "*.jsx", "*.ts", "*.tsx" },
   command = "Neoformat prettier",
 })
